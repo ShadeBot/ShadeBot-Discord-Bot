5fd543b05f0328a7bae73a8d2cbf0866a1397369
Alipoodle M <ali.poodle12@gmail.com>
2017-09-25 18:25:34 +0100

    Add files via upload

---
+++ installer.bat
@@ -0,0 +1,32 @@
+@echo off
+title Node.js Modules Installer
+
+echo / ---------------------------------------------- /
+echo           ShadeBot created by Alipoodle
+echo                 6 / 12 / 2016
+echo               All rights reserved.
+echo / ---------------------------------------------- /
+
+echo Installing Required Node Modules...
+echo --------------------------------------
+cd %~dp0
+cmd /c "npm i"
+echo.
+echo Done!
+echo Creating run files for Selfbot...
+echo ------------------------------------
+echo @echo off > run.bat
+echo :: Created by TheRacingLion (https://github.com/TheRacingLion) [ 6 / 12 / 2016 ] >> run.bat
+echo :: -*Read LICENSE to know more about permissions*- >> run.bat
+echo title Discord ShadeBot >> run.bat
+echo :START >> run.bat
+echo node serverbot.js >> run.bat
+echo goto START >> run.bat
+echo "run.bat" File Created.
+echo ------------------------------------
+echo Deleting unwanted files...
+echo ------------------------------------
+del "%~f0"
+echo Closing...
+timeout /t 5
+exit

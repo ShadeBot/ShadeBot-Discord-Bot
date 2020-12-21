4c9e5176139e57af10e0a0ec86e4b3ffa9e7a453
1314 <67989471+Vir4k@users.noreply.github.com>
2020-12-21 13:09:12 +0700

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -2,9 +2,9 @@
 title Node.js Modules Installer
 
 echo / ---------------------------------------------- /
-echo           ShadeBot created by Alipoodle
-echo          Installer made by TheRacingLion
-echo                 6 / 12 / 2016
+echo           Wyvern created by Virak
+echo          Installer made by Virak
+echo                 26 / 6 / 2020
 echo               All rights reserved.
 echo / ---------------------------------------------- /
 
@@ -17,12 +17,12 @@ echo Done!
 echo Creating run files for Selfbot...
 echo ------------------------------------
 echo @echo off > run.bat
-echo :: Script created by TheRacingLion (https://github.com/TheRacingLion) [ 6 / 12 / 2016 ] >> run.bat
-echo :: Bot created by Alipoodle (https://github.com/Alipoodle) [ 28 / 09 / 2017 ] >> run.bat
+echo :: Script created by Virak (https://github.com/Vir4k) [ 6 / 12 / 2020] >> run.bat
+echo :: Bot created by Virak (https://github.com/Vir4k) [ 26 / 06 / 2020 ] >> run.bat
 echo :: -*Read LICENSE to know more about permissions*- >> run.bat
-echo title Discord ShadeBot >> run.bat
+echo title Wyvern >> run.bat
 echo :START >> run.bat
-echo node app.js >> run.bat
+echo node index.js >> run.bat
 echo goto START >> run.bat
 echo "run.bat" File Created.
 echo ------------------------------------
